f0c305b2a1a9c09453c76b85b45f2f5a87a61a2a
Agus Supriyadi <agus.supriyadi@support.bappenas.go.id>
2012-10-16 13:26:58 +0700

    initial project import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cannes::Application.config.secret_token = '2b0b526b4b67c24af4c62eb310e36ab60c49ccbf7bcf7f5671686cbc947edb96635ebfe7e2b32457fad268e265ba51c7f51c9f1a03fa37a0f1f9847707b8c56f'
